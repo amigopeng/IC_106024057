1099177e3bbe3b7ba770c7428e83f2b58df8c4ff
Amber <amugopeng0800@gmail.com>
2017-09-25 14:31:06 +0800

    ｗｓｗ

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("106024057彭柔慈");// write your code here
+    System.out.println("資傳系");}
+}
